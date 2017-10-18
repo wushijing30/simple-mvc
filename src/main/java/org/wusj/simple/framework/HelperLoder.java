b3462de05e81e2ac9e5943689978c0745309ee5d
wushijing30 <wushijing30@163.com>
2017-10-18 23:48:10 +0800

    初始化加载相应的Helpper类

--- src/main/java/org/wusj/simple/framework/HelperLoder.java
+++ src/main/java/org/wusj/simple/framework/HelperLoder.java
@@ -1,7 +1,26 @@
 package org.wusj.simple.framework;
 
+import org.wusj.simple.framework.Utils.ClassUtil;
+import org.wusj.simple.framework.helper.BeanHelper;
+import org.wusj.simple.framework.helper.ClassHelper;
+import org.wusj.simple.framework.helper.ControllerHelper;
+import org.wusj.simple.framework.helper.IocHelper;
+
 /**
+ * 加载相应的Helpper类
  * Created by wusj on 2017/10/17.
  */
-public class HelperLoder {
+public final class HelperLoder {
+
+    public static void init() {
+        Class<?>[] classList = {
+                ClassHelper.class,
+                BeanHelper.class,
+                IocHelper.class,
+                ControllerHelper.class
+        };
+        for (Class<?> cls : classList) {
+            ClassUtil.loadClass(cls.getName());
+        }
+    }
 }
