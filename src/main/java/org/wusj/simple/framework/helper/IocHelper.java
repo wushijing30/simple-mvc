75c357d8cc0bb89a79cfd11e30d7dcf5cefd5939
wushijing30 <wushijing30@163.com>
2017-10-18 23:50:00 +0800

    添加依赖注入工具类，获取所有的Bean类与Bean实例之间的映射关系

--- src/main/java/org/wusj/simple/framework/helper/IocHelper.java
+++ src/main/java/org/wusj/simple/framework/helper/IocHelper.java
@@ -1,7 +1,44 @@
 package org.wusj.simple.framework.helper;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.wusj.simple.framework.Utils.ReflectionUtil;
+import org.wusj.simple.framework.mvc.annotation.Inject;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
 /**
+ * 依赖注入助手类
  * Created by wusj on 2017/10/8.
  */
-public class IocHelper {
+public final class IocHelper {
+    static {
+        //获取所有的Bean类与Bean实例之间的映射关系
+        Map<Class<?>, Object> beanMap = BeanHelper.getBeanMap();
+        if (!beanMap.isEmpty()) {
+            //遍历BeanMap
+            for (Map.Entry<Class<?>, Object> beanEntry : beanMap.entrySet()) {
+                Class<?> beanClass = beanEntry.getKey();
+                Object beanInstance = beanEntry.getValue();
+                //获取Bean类定义的所有成员变量
+                Field[] beanFields = beanClass.getDeclaredFields();
+                if (ArrayUtils.isNotEmpty(beanFields)) {
+                    for (Field beanField : beanFields) {
+                        //判断当前Bean的成员是否带有Inject注解
+                        if (beanField.isAnnotationPresent(Inject.class)) {
+                            //在Bean Map中获取Bean Field 对应的实例
+                            Class<?> beanFieldClass = beanField.getType();
+                            Object beanFieldInstance = beanMap.get(beanFieldClass);
+                            if (beanFieldInstance != null) {
+                                //通过反射实例化Bean Field的值
+                                ReflectionUtil.setField(beanInstance, beanField, beanFieldInstance);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
 }
