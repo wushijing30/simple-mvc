b645ae7011c1663d0fc916ce25c94b9cccd4eec0
wushijing30 <wushijing30@163.com>
2017-10-18 23:46:46 +0800

    添加配置常量类

--- src/main/java/org/wusj/simple/framework/ConfigConstant.java
+++ src/main/java/org/wusj/simple/framework/ConfigConstant.java
@@ -4,4 +4,15 @@ package org.wusj.simple.framework;
  * Created by wusj on 2017/8/20.
  */
 public class ConfigConstant {
+    public static final String CONFIG_FILE = "simple.properties";
+
+    public static final String JDBC_DRIVER = "simple.framework.jdbc.driver";
+    public static final String JDBC_URL = "simple.framework.jdbc.url";
+    public static final String JDBC_USERNAME = "simple.framework.jdbc.username";
+    public static final String JDBC_PASSWORD = "simlpe.framework.jdbc.password";
+
+    public static final String APP_BASE_PACKAGE = "simlpe.framework.app.base_package";
+    public static final String APP_JSP_PATH = "simlpe.framework.app.jsp_path";
+    public static final String APP_ASSET_PATH = "simlpe.framework.app.asset_path";
+
 }
